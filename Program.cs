48acc0bbd5a42684dcc12ab599a6ce43d91d29cf
macavall <43223084+macavall@users.noreply.github.com>
2024-05-16 20:07:48 +0000

    update program.cs

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-var host = new HostBuilder()
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureServices(services =>
+    {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
     .Build();
 
-host.Run();
+        host.Run();
+    }
+}
